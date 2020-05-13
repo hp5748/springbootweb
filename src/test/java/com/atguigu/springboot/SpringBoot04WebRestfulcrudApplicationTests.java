80ed113d80e6c0e252ab76fc36a72fcff33c29dd
He Ping <hpdream@qq.com>
2020-05-13 23:33:50 +0800

    my springboot web

--- src/test/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplicationTests.java
+++ src/test/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplicationTests.java
@@ -2,6 +2,9 @@ package com.atguigu.springboot;
 
 import com.atguigu.springboot.servise.UserService;
 import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootTest
 @ComponentScan("com.atguigu.springboot")
 class SpringBoot04WebRestfulcrudApplicationTests {
-
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Test
     void contextLoads() {
         System.out.println("sss");
@@ -19,8 +22,16 @@ class SpringBoot04WebRestfulcrudApplicationTests {
     private UserService userService;
     @Test
     void selectTest(){
+       // System.out.println(userService.selectUser("admin").toString());
+     for(int i=0;i<10000;i++)
+     {
+         logger.trace("这是入职",i);
+         logger.debug("debug",i);
+         logger.info("info",i);
+         logger.error("ss",i);
+     }
 
-        System.out.println(userService.selectUser("admin").toString());
+
     }
 
 
